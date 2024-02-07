4a086db90a760985fb252be471cf34519b78f8e7
nicolasAguero99 <nico.agueroo99@gmail.com>
2024-02-07 16:58:48 -0300

    Add loader for paid/unpaid

--- src/components/pay-button.tsx
+++ src/components/pay-button.tsx
@@ -1,16 +1,22 @@
 'use client'
 
+import { useState } from 'react'
 import { useRouter } from 'next/navigation'
 
 // Constants
 import { API_URL } from '@/constants/constants'
 
+// Components
+import Loader from './loader'
+
 export default function PayButton ({ memberId, paid }: { memberId: number, paid: boolean }): JSX.Element {
   const router = useRouter()
+  const [isDeleting, setIsDeleting] = useState(false)
   const textButton = paid ? 'Paid' : 'Unpaid'
   const color = paid ? 'text-green-500' : 'text-red-500'
 
   const handlePaidToggle = async (): Promise<void> => {
+    setIsDeleting(true)
     await fetch(`${API_URL}/invoices/${memberId}`, {
       method: 'PUT',
       body: JSON.stringify({ paid: !paid }),
@@ -22,6 +28,12 @@ export default function PayButton ({ memberId, paid }: { memberId: number, paid:
   }
 
   return (
-    <button onClick={() => { void handlePaidToggle() }} className={`${color} underline`}>{textButton}</button>
+    <>
+      {
+        isDeleting
+          ? <Loader size='size-5' />
+          : <button onClick={() => { void handlePaidToggle() }} className={`${color} underline`}>{textButton}</button>
+      }
+    </>
   )
 }
